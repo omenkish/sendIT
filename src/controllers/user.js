8653a70f2a784503862ac89cf1643025916309d9
omenkish <omenkish@gmail.com>
2018-11-07 23:56:06 +0100

    create controller to GET all parcels by user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,5 @@
 import UserModel from '../models/userModel';
+import ParcelModel from '../models/parcelOrder';
 
 const User = {
   create (req, res) {
@@ -9,6 +10,16 @@ const User = {
       const user = UserModel.create(req.body);
       return res.status(201).send(user);
     }
+  },
+
+  getUserParcels(req, res) {
+    const parcels = ParcelModel.findAll();
+    const userParcels = parcels.filter(parcel => parcel.userId === req.params.id);
+
+    if(!userParcels){
+      return res.status(404).send('message: No parcel found for this user.')
+    }
+    return res.status(200).send(userParcels)
   }
 };
 

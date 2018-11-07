9a9f6957f0ea8e087d695503038a88db6c35b2fa
omenkish <omenkish@gmail.com>
2018-11-07 10:39:28 +0100

    Define create parcel controller

---
+++ src/controllers/parcel.js
@@ -0,0 +1,14 @@
+import ParcelOrderModel from '../models/parcelOrder';
+
+const ParcelOrder = {
+  create (req, res) {
+    if(!req.body.orderNo && !req.body.address && !req.body.presentLocation && !req.body.status && !req.body.price && !req.body.description){
+      return res.status(400).send('message: All fields are required!');
+    }
+    const parcelOrder = ParcelOrderModel.create(req.body);
+    return res.status(201).send(parcelOrder);
+  }
+
+};
+
+export default ParcelOrder;

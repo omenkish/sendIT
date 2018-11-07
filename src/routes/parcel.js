289e0afbc5b5210aa29dc59521bca61bbc327de0
omenkish <omenkish@gmail.com>
2018-11-07 10:56:51 +0100

    create GET all parcels route

--- src/routes/parcel.js
+++ src/routes/parcel.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import ParcelOrder from '../controllers/parcel';
 
 router.post('', ParcelOrder.create);
+router.get('', ParcelOrder.getAll);
 
 export default router;

b8de175414c235f64be4317fc5910e6ef554734d
omenkish <omenkish@gmail.com>
2018-11-07 20:44:45 +0100

    update server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import parcel from './src/routes/parcel';
+import user from './src/routes/user';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use('/api/v1/parcels', parcel);
+app.use('/api/v1/users', user);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

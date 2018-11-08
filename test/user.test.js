1892fd256e2af58f360d61438bd947f257f0ba26
omenkish <omenkish@gmail.com>
2018-11-08 10:51:53 +0100

    refactor GET all parcels by user

--- test/user.test.js
+++ test/user.test.js
@@ -45,6 +45,31 @@ describe('User End points', () => {
           expect(err.status).to.equal(404)
         }
       })
+    });
+
+    it('should respond with all parcel orders by the user ', () => {
+      return request(server)
+      .get(`/api/v1/users/${user.id}/parcels`)
+      .then(res => {
+        expect(res.status).to.equal(200);
+        expect(res.body.length).to.equal(0);
+      })
+      .catch(err => {
+        if(err){
+          expect(err.status).to.equal(404)
+        }
+      })
+    });
+
+    it('should return no parcel to the user, and status code 404 if the user does not exist ', () => {
+      return request(server)
+      .get(`/api/v1/users/${user.id + 3}/parcels`)
+      .then(res => {
+        expect(res.status).to.equal(404);
+      })
+      .catch(err => {
+        
+      });
     })
   })
 });

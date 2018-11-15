9f6f54cf94b64e787ffe22181d4c6d72f50319d6
omenkish <omenkish@gmail.com>
2018-11-15 20:32:38 +0100

    update profile.html

--- UI/assets/js/custom.js
+++ UI/assets/js/custom.js
@@ -1,10 +1,12 @@
 function display() {
   const form  = document.getElementById("profile-form");
+  const button = document.getElementById("edit");
   let allElements = form.elements;
   for (let i = 0, l = allElements.length; i < l; i++) {
       
         allElements[i].disabled=false;
   }
+  button.classList.add("hide");
 }
 
 function confirmDelete(){

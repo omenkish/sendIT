ce17bdb668e0a3b5f9e97d59fe0d81c255c9eaae
omenkish <omenkish@gmail.com>
2018-11-18 15:08:37 +0100

    update test files

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,79 +7,68 @@ class User {
    * 
    * @returns {object} user object 
    */
-  static create() {
-    return (req, res) => {
+  static create(req, res) {
       const user = UserModel.create(req.body);
-      return res.status(201).send(user);
-    }
+      return res.status(201).send(user);   
   }
 
   /**
    * 
    * @returns {array of objects} user parcels objects
    */
-  static getUserParcels() {
-    return (req, res) => {
-      const parcels = ParcelModel.findAll();
-      const userParcels = parcels.filter(parcel => parcel.userId === req.params.id);
+  static getUserParcels(req, res) {
+    const parcels = ParcelModel.findAll();
+    const userParcels = parcels.filter(parcel => parcel.userId === req.params.id);
 
-      if (!userParcels) {
-        return res.status(404).send('message: No parcel found for this user.')
-      }
-      return res.status(200).send(userParcels)
+    if (!userParcels) {
+      return res.status(404).send('message: No parcel found for this user.')
     }
+    return res.status(200).send(userParcels)
   }
 
   /**
    * 
    * @returns {array of objects} user objects
    */
-  static getUsers() {
-    return (req, res) => {
-      const users = UserModel.findUsers();
-      return res.status(200).send(users);
-    }
+  static getUsers(req, res) {
+    const users = UserModel.findUsers();
+    return res.status(200).send(users);  
   }
 
   /**
    * 
    * @returns {object} user object 
    */
-  static getUser() {
-    return (req, res) => {
-      const user = UserModel.findUser(req.params.id);
-      if (!user) {
-        return res.status(404).send('message : No user with this Id found');
-      }
-      return res.status(200).send(user);
+  static getUser(req, res) {
+    const user = UserModel.findUser(req.params.id);
+    if (!user) {
+      return res.status(404).send('message : No user with this Id found');
     }
+    return res.status(200).send(user);
+    
   }
 
   /** method to delete a user */
-  static delete() {
-    return (req, res) => {
-      const user = UserModel.findUser(req.params.id);
-      if (!user) {
-        return res.status(404).send({ 'message': 'User not found' });
-      }
-      const ref = UserModel.delete(req.params.id);
-      return res.status(204).send(ref);
+  static delete(req, res) {
+    const user = UserModel.findUser(req.params.id);
+    if (!user) {
+      return res.status(404).send({ 'message': 'User not found' });
     }
+    const ref = UserModel.delete(req.params.id);
+    return res.status(204).send(ref);
   }
 
   /**
    * 
    * @returns {object} user object 
    */
-  static update(){
-    return (req, res) => {
-      const user = UserModel.findUser(req.params.id);
-      if (!user) {
-        return res.status(404).send({ 'message': ' User not found' })
-      }
-      const updatedUser = UserModel.update(req.params.id, req.body);
-      return res.status(200).send(updatedUser);
+  static update(req, res){
+    const user = UserModel.findUser(req.params.id);
+    if (!user) {
+      return res.status(404).send({ 'message': ' User not found' })
     }
+    const updatedUser = UserModel.update(req.params.id, req.body);
+    return res.status(200).send(updatedUser);
   };
   
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 import User from '../controllers/user';
 
-router.post('/', User.create());
-router.get('/:id/parcels', User.getUserParcels());
-router.get('/', User.getUsers());
-router.get('/:id', User.getUser());
-router.put('/:id/cancel', User.delete());
-router.put('/:id', User.update())
+router.post('/', User.create);
+router.get('/:id/parcels', User.getUserParcels);
+router.get('/', User.getUsers);
+router.get('/:id', User.getUser);
+router.put('/:id/cancel', User.delete);
+router.put('/:id', User.update)
 
 export default router;
